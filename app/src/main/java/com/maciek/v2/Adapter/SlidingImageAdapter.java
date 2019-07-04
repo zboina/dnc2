79b6028de62cc3af3b9bf0a8e8f07bb7eac12fc6
Michal <zboina@gmail.com>
2019-07-04 17:30:39 +0200

    Drobne poprawki + obracanie zdjecia z aparatu

--- app/src/main/java/com/maciek/v2/Adapter/SlidingImageAdapter.java
+++ app/src/main/java/com/maciek/v2/Adapter/SlidingImageAdapter.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.media.ExifInterface;
 import android.os.Parcelable;
 import android.support.v4.view.PagerAdapter;
 import android.util.DisplayMetrics;
@@ -87,6 +89,68 @@ public class SlidingImageAdapter extends PagerAdapter {
     }
 
 
+//    private void setPic(ImageView imageView, String currentPhotoPath, View view) {
+//        // Get the dimensions of the View
+//        DisplayMetrics displayMetrics = new DisplayMetrics();
+//        ((Activity) context).getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+//        int targetH = displayMetrics.heightPixels;
+//        int targetW = displayMetrics.widthPixels;
+//
+//        // Get the dimensions of the bitmap
+//        BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+//        bmOptions.inJustDecodeBounds = true;
+//        BitmapFactory.decodeFile(currentPhotoPath, bmOptions);
+//        int photoW = bmOptions.outWidth;
+//        int photoH = bmOptions.outHeight;
+//
+//        // Determine how much to scale down the image
+//        int scaleFactor = Math.min(photoW / targetW, photoH / targetH);
+//
+//        // Decode the image file into a Bitmap sized to fill the View
+//        bmOptions.inJustDecodeBounds = false;
+//        bmOptions.inSampleSize = scaleFactor;
+//        bmOptions.inPurgeable = true;
+//
+//        Bitmap bitmap = BitmapFactory.decodeFile(currentPhotoPath, bmOptions);
+//
+//        ExifInterface ei = new ExifInterface(currentPhotoPath);
+//        int orientation = ei.getAttributeInt(ExifInterface.TAG_ORIENTATION,
+//                ExifInterface.ORIENTATION_UNDEFINED);
+//
+//        Bitmap rotatedBitmap = null;
+//        switch(orientation) {
+//
+//            case ExifInterface.ORIENTATION_ROTATE_90:
+//                rotatedBitmap = rotateImage(bitmap, 90);
+//                break;
+//
+//            case ExifInterface.ORIENTATION_ROTATE_180:
+//                rotatedBitmap = rotateImage(bitmap, 180);
+//                break;
+//
+//            case ExifInterface.ORIENTATION_ROTATE_270:
+//                rotatedBitmap = rotateImage(bitmap, 270);
+//                break;
+//
+//            case ExifInterface.ORIENTATION_NORMAL:
+//            default:
+//                rotatedBitmap = bitmap;
+//        }
+//
+//
+//        imageView.setImageBitmap(bitmap);
+//    }
+
+
+//    public static Bitmap rotateImage(Bitmap source, float angle) {
+//        Matrix matrix = new Matrix();
+//        matrix.postRotate(angle);
+//        return Bitmap.createBitmap(source, 0, 0, source.getWidth(), source.getHeight(),
+//                matrix, true);
+//    }
+
+
+
     private void setPic(ImageView imageView, String currentPhotoPath, View view) {
         // Get the dimensions of the View
         DisplayMetrics displayMetrics = new DisplayMetrics();
@@ -110,7 +174,47 @@ public class SlidingImageAdapter extends PagerAdapter {
         bmOptions.inPurgeable = true;
 
         Bitmap bitmap = BitmapFactory.decodeFile(currentPhotoPath, bmOptions);
-        imageView.setImageBitmap(bitmap);
-    }
 
+        int orientation = 0;
+        try {
+            ExifInterface ei = new ExifInterface(currentPhotoPath);
+            orientation = ei.getAttributeInt(ExifInterface.TAG_ORIENTATION,
+                    ExifInterface.ORIENTATION_UNDEFINED);
+        } catch (Exception e) {
+            //mozna logowac ten blad np. Mozna tez tej ponizszej linijki nie dodawc bo ona i tak sluzy tylko do monitorowanie pozniejszego zachowania
+            //log.error("blad przy obracaniu", e);
+        }
+
+
+        Bitmap rotatedBitmap = null;
+        switch (orientation) {
+
+            case ExifInterface.ORIENTATION_ROTATE_90:
+                rotatedBitmap = rotateImage(bitmap, 90);
+                break;
+
+            case ExifInterface.ORIENTATION_ROTATE_180:
+                rotatedBitmap = rotateImage(bitmap, 180);
+                break;
+
+            case ExifInterface.ORIENTATION_ROTATE_270:
+                rotatedBitmap = rotateImage(bitmap, 270);
+                break;
+
+            case ExifInterface.ORIENTATION_NORMAL:
+            default:
+                rotatedBitmap = bitmap;
+
+
+                imageView.setImageBitmap(rotatedBitmap);
+
+        }
+
+    }
+    private Bitmap rotateImage (Bitmap source,float angle){
+        Matrix matrix = new Matrix();
+        matrix.postRotate(angle);
+        return Bitmap.createBitmap(source, 0, 0, source.getWidth(), source.getHeight(),
+                matrix, true);
+    }
 }
